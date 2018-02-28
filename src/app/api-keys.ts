4cdebe5d0936f982415ee08dc89b7ce05e153c61
Byron Chang and Kate Trahan <byron@>
2018-02-28 09:07:53 -0800

    add update/add delete

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export var masterFirebaseConfig = {
+   apiKey: "AIzaSyBAPLRWYoIW6U2ZikdkQ44Ju4ySbCi932E",
+   authDomain: "onlinestore-c5222.firebaseapp.com",
+   databaseURL: "https://onlinestore-c5222.firebaseio.com",
+   projectId: "onlinestore-c5222",
+   storageBucket: "onlinestore-c5222.appspot.com",
+   messagingSenderId: "1003920320774"
+}
